7119f2549c547ac44d4d9a997548eef5afcb8e0f
HetDesai45 <108257834+HetDesai45@users.noreply.github.com>
2024-07-05 18:18:09 +0530

    M

--- Level2_Task2/Frontend/src/Redux/actions/jobAction.jsx
+++ Level2_Task2/Frontend/src/Redux/actions/jobAction.jsx
@@ -1,11 +1,18 @@
-import { JOB_LOAD_REQUEST } from "../constants/jobconstant"
+import { JOB_LOAD_FAIL, JOB_LOAD_REQUEST, JOB_LOAD_SUCCESS } from "../constants/jobconstant"
 import axios from 'axios';
 
 export const jobLoadAction = (pageNumber, keyword='', cat='', location='')=>async(dispatch)=>{
   dispatch({type: JOB_LOAD_REQUEST});
   try {
-    const {data} = await axios.get('/')
+    const {data} = await axios.get(`/job/show/?pageNumber=${pageNumber}&keyword=${keyword}&cat=${cat}&location=${location}`);
+    dispatch({
+      type: JOB_LOAD_SUCCESS,
+      payload: data
+    })
   } catch (error) {
-    
+    dispatch({
+      type: JOB_LOAD_FAIL,
+      payload: error.response.data.error
+    })
   }
 }
