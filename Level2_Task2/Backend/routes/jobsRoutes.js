0ec441df8406ec2d904e18a729898ba0843d1cdc
HetDesai45 <108257834+HetDesai45@users.noreply.github.com>
2024-07-19 01:19:43 +0530

    changes

--- Level2_Task2/Backend/routes/jobsRoutes.js
+++ Level2_Task2/Backend/routes/jobsRoutes.js
@@ -3,10 +3,10 @@ const { isAuthenticated, isAdmin } = require('../middleware/auth');
 const { createJob, signlejob, updatejob, showjob, deleteJob } = require('../Controllers/jobsController');
 const router = express.Router();
 
-router.post('/job/create', isAuthenticated, createJob);
+router.post('/job/create', isAuthenticated, isAdmin, createJob);
 router.get('/job/:id', signlejob);
-router.put('/job/update/:job_id', isAuthenticated, updatejob);
-router.delete('/job/delete/:job_id', isAuthenticated, deleteJob);
+router.put('/job/update/:job_id', isAuthenticated, isAdmin, updatejob);
+router.delete('/job/delete/:job_id', isAuthenticated, isAdmin, deleteJob);
 router.get('/jobs/show',showjob);
 
 module.exports = router;
--- Level2_Task2/Backend/routes/userRoutes.js
+++ Level2_Task2/Backend/routes/userRoutes.js
@@ -3,9 +3,14 @@ const router = express.Router();
 const { allUsers, singleUser, editUser, deleteUser, createUserJobHistory } = require("../Controllers/userController");
 const { isAuthenticated, isAdmin } = require("../middleware/auth");
 
-router.get("/allusers", allUsers);
-router.get("/user/:id", singleUser);
-router.put("/user/edit/:id", editUser);
-router.delete("/user/delete/:id", deleteUser);
-router.post("/user/jobhistory", createUserJobHistory);
+router.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+
+router.get("/allusers", isAuthenticated, isAdmin, allUsers);
+router.get("/user/:id", isAuthenticated, singleUser);
+router.put("/user/edit/:id", isAuthenticated, editUser);
+router.delete("/user/delete/:id", isAuthenticated, isAdmin, deleteUser);
+router.post("/user/jobhistory", isAuthenticated, createUserJobHistory);
 module.exports = router;
