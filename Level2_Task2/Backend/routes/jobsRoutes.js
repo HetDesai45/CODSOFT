5d26efe4d485f2b01331225dd8869cd8828536a5
HetDesai45 <108257834+HetDesai45@users.noreply.github.com>
2024-07-19 01:09:33 +0530

    changes

--- Level2_Task2/Backend/routes/jobsRoutes.js
+++ Level2_Task2/Backend/routes/jobsRoutes.js
@@ -3,10 +3,10 @@ const { isAuthenticated, isAdmin } = require('../middleware/auth');
 const { createJob, signlejob, updatejob, showjob, deleteJob } = require('../Controllers/jobsController');
 const router = express.Router();
 
-router.post('/job/create', createJob);
+router.post('/job/create', isAuthenticated, createJob);
 router.get('/job/:id', signlejob);
-router.put('/job/update/:job_id', updatejob);
-router.delete('/job/delete/:job_id', deleteJob);
+router.put('/job/update/:job_id', isAuthenticated, updatejob);
+router.delete('/job/delete/:job_id', isAuthenticated, deleteJob);
 router.get('/jobs/show',showjob);
 
 module.exports = router;
