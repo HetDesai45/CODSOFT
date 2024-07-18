2e2043f6116741329878cd5787bc49644294d523
HetDesai45 <108257834+HetDesai45@users.noreply.github.com>
2024-07-19 00:47:43 +0530

    changes

--- Level2_Task2/Backend/routes/userRoutes.js
+++ Level2_Task2/Backend/routes/userRoutes.js
@@ -3,14 +3,9 @@ const router = express.Router();
 const { allUsers, singleUser, editUser, deleteUser, createUserJobHistory } = require("../Controllers/userController");
 const { isAuthenticated, isAdmin } = require("../middleware/auth");
 
-router.get("/", (req, res) => {
-  res.send("Hello");
-});
-
-
-router.get("/allusers", isAuthenticated, isAdmin, allUsers);
-router.get("/user/:id", isAuthenticated, singleUser);
-router.put("/user/edit/:id", isAuthenticated, editUser);
-router.delete("/user/delete/:id", isAuthenticated, isAdmin, deleteUser);
-router.post("/user/jobhistory", isAuthenticated, createUserJobHistory);
+router.get("/allusers", allUsers);
+router.get("/user/:id", singleUser);
+router.put("/user/edit/:id", editUser);
+router.delete("/user/delete/:id", deleteUser);
+router.post("/user/jobhistory", createUserJobHistory);
 module.exports = router;
