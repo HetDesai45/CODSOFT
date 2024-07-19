79bdfa3589749af729872bc4cd49ea700fc9d340
HetDesai45 <108257834+HetDesai45@users.noreply.github.com>
2024-07-19 16:52:25 +0530

    update

--- Level2_Task2/Backend/Controllers/authController.js
+++ Level2_Task2/Backend/Controllers/authController.js
@@ -46,15 +46,11 @@ exports.signin = async (req, res, next) => {
 
 const sendTokenResponse = async (user, codeStatus, res) => {
   const token = await user.getJwtToken();
-  res
+  return res
     .status(codeStatus)
     .cookie("token", token, {
       maxAge: 60 * 60 * 1000,
-      httpOnly: true,
-      secure: true,
-      smaeSite: "None",
-      domain: "codsoft-pxih.onrender.com",
-      path: "/",
+      httpOnly: true
     })
     .json({
       success: true,
